44d3a4cf1fa99b179928a5009c30eeb1a32b9ca3
Ivan Kaurov <crazyanimalcz@gamil.com>
2025-01-14 18:20:45 +0300

    Continue

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
 vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.", 'Москва',
                   "Требования: опыт работы от 3 лет...")
 
-# Сохранение информации о вакансиях в файл
 json_saver = JSONSaver()
 json_saver.add_vacancy(vacancy)
 json_saver.delete_vacancy(vacancy)
